5dca1b92b43494404365da36e5e38daa358fde33
Karla Ramirez <karlar1824@gmail.com>
2023-02-26 00:19:35 -0500

    updated

--- Culmination2023/Culmination2023.ino
+++ Culmination2023/Culmination2023.ino
@@ -1,13 +1,17 @@
 #include <Wire.h>
-#include "Adafruit_MPR121.h"
+#include <MPR121_Datastream.h>
+#include <MPR121.h>
 
-Adafruit_MPR121 cap_sense = Adafruit_MPR121();
 
-#define MPR121_ADDR 0x5A
+
+#define MPR121_ADDR 0x5C
 #define PRINT_DELAY 200
 
-uint32_t sensedPoints[2][2];
-volatile bool readFlag = false;
+const uint8_t MPR121_INT = 4;
+uint32_t sensedPoints[2][2]; // this is reading as bytes
+//volatile bool readFlag = false;
+
+const bool MPR121_DATASTREAM_ENABLE = false; //not sending data back
 
 // Rows are pin 0-5, cols are pins 6-11
 /*
@@ -30,55 +34,119 @@ Pin 6 | 7 | 8 | 9 | 10 | 11
 
 void setup()
 {
-    Serial.begin(115200);
-    while(!Serial);
-    if(!cap_sense.begin(MPR121_ADDR))
-    {
-        //Serial.println("Error setting up MPR121");
-        while(1);
+    Serial.begin(115200); // the baud rate
+    
+//    while(!Serial); // wait 
+//    pinMode(LED_BUILTIN, OUTPUT);
+    
+    if (!MPR121.begin(MPR121_ADDR)) {
+    Serial.println("error setting up MPR121");
+    switch (MPR121.getError()) {
+      case NO_ERROR:
+        Serial.println("no error");
+        break;
+      case ADDRESS_UNKNOWN:
+        Serial.println("incorrect address");
+        break;
+      case READBACK_FAIL:
+        Serial.println("readback failure");
+        break;
+      case OVERCURRENT_FLAG:
+        Serial.println("overcurrent on REXT pin");
+        break;
+      case OUT_OF_RANGE:
+        Serial.println("electrode out of range");
+        break;
+      case NOT_INITED:
+        Serial.println("not initialised");
+        break;
+      default:
+        Serial.println("unknown error");
+        break;
     }
-    attachInterrupt(digitalPinToInterrupt(2), updateSensedFlagSet, FALLING);
-    //Serial.println("Ready to sense");
+    while (1);
+  }
+
+  MPR121.setInterruptPin(MPR121_INT);
+
+  if (MPR121_DATASTREAM_ENABLE) {
+    MPR121.restoreSavedThresholds();
+    MPR121_Datastream.begin(&Serial);
+  } else {
+    MPR121.setTouchThreshold(40);  // this is the touch threshold - setting it low makes it more like a proximity trigger, default value is 40 for touch
+    MPR121.setReleaseThreshold(20);  // this is the release threshold - must ALWAYS be smaller than the touch threshold, default value is 20 for touch
+  }
+
+  MPR121.setFFI(FFI_10);
+  MPR121.setSFI(SFI_10);
+  MPR121.setGlobalCDT(CDT_4US);  // reasonable for larger capacitances
+  
+  digitalWrite(LED_BUILTIN, HIGH);  // switch on user LED while auto calibrating electrodes
+  delay(1000);
+  MPR121.autoSetElectrodes();  // autoset all electrode settings
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
-    /*if(readFlag)
-    {
-        readFlag = false;
-        updateSensed();
-    }*/
-    updateSensed();
+  MPR121.updateAll();// this is reading the values of the chip 
+   
+//   for (int i = 0; i < 4; i++) {
+//    if (MPR121.isNewTouch(i)) { // this is letting the board know if i is being touch from 0-4 electrodes then write "i is touched"
+//      Serial.print("electrode ");
+//      Serial.print(i, DEC);
+//      Serial.println(" was just touched");
+//    } else if (MPR121.isNewRelease(i)) { // else it reads that its not being touched anymore
+//      Serial.print("electrode ");
+//      Serial.print(i, DEC);
+//      Serial.println(" was just released");
+//    }
+  
+  if (MPR121_DATASTREAM_ENABLE) {
+    MPR121_Datastream.update();
+  }
+  
+//    if(readFlag)
+//    {
+//        readFlag = false;
+//        updateSensed();
+//    }
+//    updateSensed();
     printTable();
     delay(PRINT_DELAY);
+//   }
 }
-
 void printTable()
 {
+
     Serial.println("TABLE");
-    for(int i=0; i<2; i++)
+    for(int i=0; i<6; i++)
     {
-        for(int j=0; j<2; j++)
+        for(int j=0; j<6; j++)
         {
-            if(j != 1) Serial.print(String(sensedPoints[i][j]) + " | ");
+            if(MPR121.isNewTouch(j != 6)) Serial.print(String(sensedPoints[i][j]) + " | ");
+            
             else Serial.println(String(sensedPoints[i][j]));
         }
-        //if(i == 5) Serial.println("\n-------------------\n");
+//        //if(i == 5) Serial.println("\n-------------------\n");
     }
     
 }
 
-void updateSensedFlagSet()
-{
-    readFlag = true;
-}
 
-void updateSensed()
-{
-    //Serial.println("Reading");
-    for(uint8_t i=0; i<2; i++)
-    {
-        for(uint8_t j=0; j<2; j++) sensedPoints[i][j] = cap_sense.filteredData(i) * cap_sense.filteredData(j + 6);
-    }
-    //Serial.println("Table updated");
-}
+
+
+//void updateSensedFlagSet()
+//{
+//    readFlag = true;
+//}
+
+//void updateSensed()
+//{
+//    //Serial.println("Reading");
+//    for(uint8_t i=0; i<2; i++) // means a byte 
+//    {
+//        for(uint8_t j=0; j<2; j++) sensedPoints[i][j] = cap_sense.filteredData(i) * cap_sense.filteredData(j + 6);
+//    }
+//    //Serial.println("Table updated");
+//}
